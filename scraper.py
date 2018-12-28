eb07882a60907b23f368e26242f313d04ed815cb
Samuel Polo <samuelpp1996@hotmail.com>
2018-12-27 22:21:04 -0500

    First commit: Web Scrapper.

---
+++ scraper.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen
+
+from urllib.error import HTTPError
+
+from urllib.error import URLError
+
+from bs4 import BeautifulSoup
+
+try:
+
+    html = urlopen("https://www.python.org/")
+
+except HTTPError as e:
+
+    print(e)
+
+except URLError:
+
+    print("Server down or incorrect domain")
+
+else:
+
+    res = BeautifulSoup(html.read(), "html5lib")
+
+    tags = res.findAll("h2", {"class": "widget-title"})
+
+    for tag in tags:
+
+        print(tag.getText())
